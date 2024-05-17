d36c546b7cfe0b608624c5349152087e788b8a28
roman-makhov <roman-makhov@supports-MacBook-Air.local>
2024-05-17 23:58:39 +0300

    fix: biome fix

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-
 import { MainLayout } from "@/components/layouts/main";
+
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
